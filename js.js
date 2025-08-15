2a82fcc8f094de5f4d86049ac63c6435e6caa205
B-R-CATANEO <cotocobruno31@gmail.com>
2025-08-15 14:11:08 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+var audio = document.getElementById('audio');
+var playButton = document.getElementById('playButton');
+var text = document.getElementById('text');
+
+function olhaOPorco() {
+    audio.play();
+    text.textContent = "É BRINCADEIRAAAAAAAAAAAAAAAAAAAAA KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
+    playButton.style.display - "none";
+}
